02d89a45d7771b937e5d01cedccb95ab786610b6
Amrendra Kumar <amarmca51@gmail.com>
2021-05-30 17:25:36 +0530

    Added first configuration

---
+++ src/components/Ingredients/IngredientForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
+import './IngredientForm.css';
+
+const IngredientForm = React.memo((props) => {
+  //const [inputTitle, setInputTitle] = useState({ title: '', amount: '' });
+
+  const [enterdtitle, setEnteredTitle] = useState('');
+  const [enteredamount, setEnteredAmount] = useState('');
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // ...
+  };
+
+  const handleTitle = (e) => {
+    setEnteredTitle( e.target.value );
+    console.log(enterdtitle)
+    //setInputTitle({ title: e.target.value });
+  };
+
+  const handleAmount = (e) => {
+    setEnteredAmount(e.target.value );
+    console.log(enteredamount)
+    // setInputTitle({ amount: e.target.value });
+  };
+  return (
+    <section className='ingredient-form'>
+      <Card>
+        <form onSubmit={submitHandler}>
+          <div className='form-control'>
+            <label htmlFor='title'>Name</label>
+            <input
+              type='text'
+              id='title'
+              value={enterdtitle}
+              onChange={handleTitle}
+            />
+          </div>
+          <div className='form-control'>  
+            <label htmlFor='amount'>Amount</label>
+            <input
+              type='number'
+              id='amount'
+              value={enteredamount}
+              onChange={handleAmount}
+            />
+          </div>
+          <div className='ingredient-form__actions'>
+            <button type='submit'>Add Ingredient</button>
+          </div>
+        </form>
+      </Card>
+    </section>
+  );
+});
+
+export default IngredientForm;
---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import Search from './Search';
+
+function Ingredients() {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients((prevIngredients) => [
+      ...prevIngredients,
+      { id: Math.random.toString(), ...ingredient },
+    ]);
+  };
+
+  return (
+    <div className='App'>
+      <IngredientForm onAddIngredient = {addIngredientHandler} />
+
+      <section>
+        <Search />
+        <IngredientList ingredients={userIngredients} />
+      </section>
+    </div>
+  );
+}
+
+export default Ingredients;
